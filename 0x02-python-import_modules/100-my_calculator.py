5d684ecd138dfbca2dc18f286f985d73c488cdb9
Abdoulkarim <amahamn@gmail.com>
2022-06-06 16:25:49 +0200

    tet

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,9 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
-    argv = sys.argv[1:]
-    arg_num = len(argv)
+    #argv = sys.argv[1:]
+
+    arg_num = len(sys.argv)
     if arg_num != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
