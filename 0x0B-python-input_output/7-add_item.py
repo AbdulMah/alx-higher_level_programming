444800d83ad99463467c2cf291fcd7b564ff0745
Abdoulkarim <amahamn@gmail.com>
2022-07-05 14:01:16 +0200

    final

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """
-Module 9-add_item
-Contains function that adds and saves to Python obj to JSON file; loads objects
-
+    Python script that adds all args to a Python List.
+    List is then saved to a file.
 """
 
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+import sys
+import json
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+file = "add_item.json"
+json_list = []
 
-contents = load_from_json_file(filename)
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
 
-save_to_json_file(contents + argv[1:], filename)
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
+
+save_to_json_file(json_list, file)
