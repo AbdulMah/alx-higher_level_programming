ca7e102b1c7717f48d2143a6470d2ec476b64e80
Abdoulkarim <amahamn@gmail.com>
2022-09-28 03:51:18 +0200

    final

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """ fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request as req
+import urllib.request 
 
+
+def fetch():
+    """fetcher"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
 
 if __name__ == "__main__":
-    with req.urlopen('https://intranet.hbtn.io/status') as response:
-        data = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(data)))
-        print("\t- content: {}".format(data))
-        print("\t- utf8 content: {}".format(data.decode("utf-8")))
+    fetch()
