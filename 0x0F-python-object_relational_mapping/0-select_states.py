e436f1d4d5ada0996f60c4494a36ccea9bbdd9c1
Abdoulkarim <amahamn@gmail.com>
2022-09-28 14:37:59 +0200

    final

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,10 +8,10 @@ Arguments:
 """
 
 
-import MySQLdb
-from sys import argv
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
     db = MySQLdb.connect(user=argv[1],
                          passwd=argv[2],
                          db=argv[3],
