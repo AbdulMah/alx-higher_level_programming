079bcee55aa882bf5ab2c61d911fb227c23d5795
Abdoulkarim <amahamn@gmail.com>
2022-09-28 14:04:03 +0200

    final

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,20 @@
 #!/usr/bin/python3
-"""List all states from the database.
-Arguments:
-    username - username to connect the mySQL
-    mysql passwd - password to connect the mySQL
-    DB name - Name of the database
-
+"""script that lists all states from the
+database hbtn_0e_0_usa
 """
 
 
-import MySQLdb
-from sys import argv
-if __name__ == "__main__":
-
-    db = MySQLdb.connect(user=argv[1],
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+    conn = MySQLdb.connect(user=argv[1],
                          passwd=argv[2],
                          db=argv[3],
                          host="localhost",
                          port=3306)
-    """Connect to a MySQL server."""
-
-    cursor = db.cursor()
-    """Used to instantiate a MySQL cursor object."""
-
-    sort = "SELECT * FROM states ORDER BY states.id ASC"
-    cursor.execute(sort)
-    """Executes the given database operation (query or command)."""
-
-    lists = cursor.fetchall()
-    """Fetches all rows of a query result set and returns a list."""
-    for row in lists:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
-    cursor.close()
-    """
-    Closes the cursor, resets all results, and ensures that the cursor object
-    has no reference to its original connection object.
-    """
-    db.close()
+    conn.close()
